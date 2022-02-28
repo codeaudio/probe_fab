583d9d7bcffa88c77394598b268a5680a8039063
Иван <ivan07m@gmail.com>
2022-02-28 10:53:24 +0300

    fix config

--- probe_fab/celery.py
+++ probe_fab/celery.py
@@ -1,10 +1,13 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'probe_fab.settings')
 
 app = Celery('probe_fab.tasks.send')
+
+app.config_from_object(settings, namespace='CELERY')
 
 app.conf.timezone = 'Europe/Moscow'
 
